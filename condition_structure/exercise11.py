49567c03c6714d79624c834d059947885794841c
Francisco Junior <fgsj.dev@outlook.com>
2025-04-03 19:57:18 -0300

    feat: number pair or odd

---
+++ condition_structure/exercise11.py
@@ -0,0 +1,12 @@
+"""
+Author: Francisco Junior
+Date: 03/04/2025 - Brazil format
+Description: Indentify if number is odd or pair
+"""
+
+number = int(input("number: "))
+
+if number % 2 == 0:
+    print("number is pair")
+else:
+    print("number is odd")
