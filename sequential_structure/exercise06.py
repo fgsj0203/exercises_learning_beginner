a06b9e7f1d63025d6916d5df6dead590dcbde4ca
Francisco Junior <fgsj.dev@outlook.com>
2025-04-03 09:25:20 -0300

    feat: calculate area of circle

---
+++ sequential_structure/exercise06.py
@@ -0,0 +1,13 @@
+"""
+Author: Francisco Junior
+Date: 03/04/2025 - Brazil format
+Description: Calculate area of circle
+"""
+
+PI = 3.14159
+
+raid_circle = float(input("what value of raid circle: "))
+
+area_circle_phormula = PI * (raid_circle**2)
+
+print("The area of circle is: %.2f cm²" % area_circle_phormula)
