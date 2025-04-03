7243baddca08eddc43fc75a08ae65c94b7d3a324
Francisco Junior <fgsj.dev@outlook.com>
2025-04-03 09:30:15 -0300

    feat: calculate area of square

---
+++ sequential_structure/exercise07.py
@@ -0,0 +1,10 @@
+"""
+Author: Francisco Junior
+Date: 03/04/2025 - Brazil format
+Description: Calculate area of squat
+"""
+
+side_square = int(input("what value of side squat: "))
+area_of_square = side_square * side_square
+
+print("Area of circle is: %.2f cm²" % area_of_square)
