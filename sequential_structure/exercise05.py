00f5ad74b59c0ead7dcc527c8ca9a38b71f7ce00
Francisco Junior <fgsj.dev@outlook.com>
2025-04-03 09:19:26 -0300

    feat: calculate size converted meters for centimeters

---
+++ sequential_structure/exercise05.py
@@ -0,0 +1,11 @@
+"""
+Author: Francisco Junior
+Date: 03/04/2025 - Brazil format
+Description: Converter size meters in centimeters
+"""
+
+size_meters = int(input("what size in meters: "))
+
+converter_size = size_meters * 100
+
+print("%d converterd for centimeters is %d cm " % (size_meters, converter_size))
