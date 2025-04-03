c1a590f0b79cb430eae8b1a681aedb384643d7a2
Francisco Junior <fgsj.dev@outlook.com>
2025-04-03 10:15:14 -0300

    feat: identify if number is positive or negative

---
+++ condition_structure/exercise02.py
@@ -0,0 +1,11 @@
+"""
+Author: Francisco Junior
+Date: 03/04/2025 - Brazil format
+Description: Identify if number is positive or negative
+"""
+
+number = int(input("number: "))
+if number >= 0:
+    print("Number is positive")
+else:
+    print("Number is negative")
