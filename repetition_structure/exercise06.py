19110fb23b43b49c7c2717067063df590d369a57
Francisco Junior <fgsj.dev@outlook.com>
2025-04-04 09:38:26 -0300

    fix: update name variable of sum numbers

--- repetition_structure/exercise06.py
+++ repetition_structure/exercise06.py
@@ -4,13 +4,13 @@ Date: 04/04/2025 - Brazil format
 Description: return sum of five numbers and average
 """
 
-sum = 0
+sum_numbers = 0
 for x in range(0, 5):
     number = int(input("number: "))
-    sum += number
+    sum_numbers += number
 
 # calculating average
-average_numbers = sum / 5
+average_numbers = sum_numbers / 5
 
-print("Sum of numbers is: %d " % sum)
+print("Sum of numbers is: %d " % sum_numbers)
 print("The average of numbers is: %d " % average_numbers)
