f44a13cae22b2961a5717bdeef15f6cfd5473012
Francisco Junior <fgsj.dev@outlook.com>
2025-04-04 09:12:18 -0300

    feat: printing numbers side to side and up down

---
+++ repetition_structure/exercise04.py
@@ -0,0 +1,11 @@
+"""
+Author: Francisco Junior
+Date: 04/04/2025 - Brazil format
+Description: printing numbers in range 0 to 20
+"""
+
+for x in range(0, 21):
+    print(x)
+
+for x in range(0, 21):
+    print(x, end=" ")
