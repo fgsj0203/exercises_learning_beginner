f03c17d958f86b4adb754fa6f8eb89d915c66d4d
Francisco Junior <fgsj.dev@outlook.com>
2025-04-04 16:11:51 -0300

    feat: list with numbers

---
+++ lists_structure/exercise01.py
@@ -0,0 +1,13 @@
+"""
+Author: Francisco Junior
+Date: 04/04/2025 - Brazil format
+Description: Printing numbers in list
+"""
+
+list_numbers = []
+
+for x in range(0, 5):
+    number = int(input("number: "))
+    list_numbers.append(number)
+
+print("The list with numbers is: ", list_numbers)
