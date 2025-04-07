3f366aeb2439fd8b0e458141382b320f6446cbef
Francisco Junior <fgsj.dev@outlook.com>
2025-04-07 10:55:10 -0300

    feat: printing string in format vertical

---
+++ String_structure/exercise03.py
@@ -0,0 +1,16 @@
+"""
+Author: Francisco Junior
+Date: 07/04/2025 - Brazil format
+Description: Printing string characters in vertical
+"""
+
+name = str(input("name: "))
+for x in name:
+    print(x)
+
+print("-------------------------------------------")
+print("second format")
+print("-------------------------------------------\n")
+
+for x in range(0, len(name)):
+    print(name[x])
