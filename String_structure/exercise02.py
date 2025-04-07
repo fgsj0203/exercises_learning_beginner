1713697f788a3b7668612e0561a7c8cf91b8b269
Francisco Junior <fgsj.dev@outlook.com>
2025-04-07 10:49:55 -0300

    feat: printing reverse string in format uppercase

---
+++ String_structure/exercise02.py
@@ -0,0 +1,10 @@
+"""
+Author: Francisco Junior
+Date: 07/04/2025 - Brazil format
+Description: Printing characters in format uppercase order reverse
+"""
+
+name = str(input("name: "))
+print(
+    name.upper()[::-1]
+)  # upper for converter string in format uppercase and ::-1 for start last character
